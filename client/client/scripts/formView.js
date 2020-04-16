cf7f0f29bcc17d73dc0091c5974ac571b48f4e50
Benjamin Barnett <ben.barnett@gmail.com>
2020-04-15 19:52:53 -0700

    Add Chatterbox solution code
    Starting from an empty prompt would be a bad idea.

--- client/client/scripts/formView.js
+++ client/client/scripts/formView.js
@@ -9,13 +9,22 @@ var FormView = {
   handleSubmit: function(event) {
     // Stop the browser from submitting the form
     event.preventDefault();
-    
-    console.log('click!');
-  },
+
+
+    var message = {
+      username: App.username,
+      text: FormView.$form.find('#message').val(),
+      roomname: Rooms.selected || 'lobby'
+    };
+
+    Parse.create(message, (data) => {
+      _.extend(message, data);
+      Messages.add(message, MessagesView.render);
+    });
+      },
 
   setStatus: function(active) {
     var status = active ? 'true' : null;
     FormView.$form.find('input[type=submit]').attr('disabled', status);
   }
-
 };
